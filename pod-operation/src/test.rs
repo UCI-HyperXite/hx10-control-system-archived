3898db83a0641585c9ccc1e20970cb969888ed2b
blair chung <154652838+pikap0ng@users.noreply.github.com>
2025-01-09 15:57:49 -0800

    log distance value before 50ms delay in test.rs

--- pod-operation/src/test.rs
+++ pod-operation/src/test.rs
@@ -4,9 +4,9 @@ use crate::components::yaw::Yaw;
 
 pub async fn read_distance(mut yaw: Yaw){
     info!("Starting Vl53L0x Test.");
-    loop{
-        let distance = yaw.read_distance();
-        tokio::time::sleep(std::time::Duration::from_millis(50)).await;
-        println!("Distance: {:/}", Distance.distance);
+    loop {
+        let distance = yaw.read_distance(); 
+        println!("Distance: {:?}", distance.distance); // log distance value 
+        tokio::time::sleep(std::time::Duration::from_millis(50)).await; // waits 50ms before reading again 
     }
 }
